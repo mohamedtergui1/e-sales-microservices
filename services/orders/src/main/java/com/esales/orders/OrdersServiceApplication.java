ab28373923c6f3d80d6fc732e95af4a81461cf20
AstaYC PS <baihmohamedamine@gmail.com>
2025-02-25 07:53:15 +0000

    drfine the database

--- services/orders/src/main/java/com/esales/orders/OrdersServiceApplication.java
+++ services/orders/src/main/java/com/esales/orders/OrdersServiceApplication.java
@@ -2,12 +2,12 @@ package com.esales.orders;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient  // Register with Eureka, not be Eureka
 public class OrdersServiceApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(OrdersServiceApplication.class, args);
 	}
-
 }
